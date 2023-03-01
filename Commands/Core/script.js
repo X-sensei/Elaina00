e1552eea088615bc23550199cab026c73b8ab872
Aries-27 <101610365+Aries-27@users.noreply.github.com>
2023-03-01 21:35:47 +0530

    Update script.js

--- Commands/Core/script.js
+++ Commands/Core/script.js
@@ -11,7 +11,22 @@ module.exports = {
         let picURL = fs.readFileSync('./Page/BG.jpg')
         let repoInfo = await axios.get('https://api.github.com/repos/FantoX001/Atlas-MD')
         let repo = repoInfo.data
-        let txt = `      🧣 *${botName}'s Script* 🧣\n\n*🎀 Total Forks:* ${repo.forks_count}\n*⭐ Total Stars:* ${repo.stargazers_count}\n*📜 License:* ${repo.license.name}\n*📁 Repo Size:* ${(repo.size/1024).toFixed(2)} MB\n*📅 Last Updated:* ${repo.updated_at}\n\n*🔗 Repo Link:* ${repo.html_url}\n\n❝ Dont forget to give a Star ⭐ to the repo. It's made with restless harkwork by *Team ATLAS*. ❞\n\n*©️ Team ATLAS- 2023*`
+        let txt = `      🧣 *${botName}*
+        
+        Hii! I'm Elaina a whatsapp bot, Developed by Atlas and Reverse engineered by Ronen to make your whatsapp more enjoyable and to do everything possible on whatsapp.
+        
+💜 *Our official support group* : https://chat.whatsapp.com/Hooxu1kZEoFKoNpdFLqXMO
+🐨 *My YouTube* : https://youtube.com/c/RonenAMVs
+🔖 *Website* : Coming soon...
+
+*About Ronen💜*
+*🎗️Name* : Ronen Singha
+*🧧Age* : Immortal heheh✨
+*♦️Country* : India🇮🇳
+*🎯Gender* : Male👨‍🦱
+*❣️Relationship status* : Single
+*🎏Hobbies* : Coding, Video Editing, Workout 💪, Love Watching Animes and Kdramas`
+`
         await Miku.sendMessage(m.from,{image:picURL, caption:txt},{quoted:m});
     }
 }
